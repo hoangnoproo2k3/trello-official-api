f3e7fa77f1bf68e58b51ec137a2a8627e7e675e2
Viet Hoang <hoangproo2624@gmail.com>
2024-05-12 23:26:42 +0700

    test res.json

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,23 +8,36 @@ const passport = require('passport')
 const CLIENT_URL = getClientUrl()
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
 
-router.get('/google/callback',
-  passport.authenticate('google',
-    { failureRedirect: '/login-fail', session: false }),
-  (req, res) => {
-    const token = generateJWT(req.user)
-    // Lưu cookie trên thiết bị đăng nhập
-    res.cookie('x-auth-cookie', token)
-    res.cookie('gg_id', req.user.id)
-    res.redirect(CLIENT_URL)
-  })
-
-router.get('/logout', (req, res) => {
-  res.clearCookie('x-auth-cookie')
-  res.clearCookie('gg_id')
-  res.redirect(`${CLIENT_URL}`)
+// Sửa đổi router.get('/google/callback', ...)
+router.get('/google/callback', passport.authenticate('google',
+  { failureRedirect: '/login-fail', session: false }),
+(req, res) => {
+  const user = req.user
+  const token = generateJWT(user)
+  res.json({ token, user })
 })
 
+// Sửa đổi router.get('/logout', ...)
+router.get('/logout', (req, res) => {
+  // Không cần xóa cookie, chỉ cần phản hồi thành công
+  res.status(200).json({ message: 'Đăng xuất thành công' })
+})
+// router.get('/google/callback',
+//   passport.authenticate('google',
+//     { failureRedirect: '/login-fail', session: false }),
+//   (req, res) => {
+//     const token = generateJWT(req.user)
+//     // Lưu cookie trên thiết bị đăng nhập
+//     res.cookie('x-auth-cookie', token)
+//     res.cookie('gg_id', req.user.id)
+//     res.redirect(CLIENT_URL)
+//   })
+
+// router.get('/logout', (req, res) => {
+//   res.clearCookie('x-auth-cookie')
+//   res.clearCookie('gg_id')
+//   res.redirect(`${CLIENT_URL}`)
+// })
 router.get('/protected', requireJwtAuth, (req, res) => {
   if (req.user) {
     res.json({ message: 'Xác thực thành công', user: req.user })
